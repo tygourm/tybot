b21238143aaa0d1b9b57ebeec73b876f3617af6c
tygourm <thomasgourmelon.dev@gmail.com>
2025-06-09 22:20:18 +0200

    📝 Embed docs in the bot

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,9 @@
+from pathlib import Path
+
 from chainlit.utils import mount_chainlit
 from fastapi import FastAPI
 from fastapi.openapi.docs import get_swagger_ui_html
-from starlette.responses import HTMLResponse
+from starlette.responses import FileResponse, HTMLResponse, Response
 
 from bot.core.settings import settings
 
@@ -14,15 +16,23 @@ app = FastAPI(
 )
 
 
-@app.get("/docs", include_in_schema=False)
+@app.get("/api-docs", include_in_schema=False)
 async def docs() -> HTMLResponse:
     return get_swagger_ui_html(
         openapi_url=app.openapi_url,
-        title=f"{settings.TITLE} Docs",
+        title=f"{settings.TITLE} API Docs",
         swagger_js_url="/public/docs/swagger-ui.js",
         swagger_css_url="/public/docs/swagger-ui.css",
         swagger_favicon_url="/public/build/favicon.svg",
     )
+
+
+@app.get("/app-docs/{path:path}", include_in_schema=False)
+async def site(path: str) -> Response:
+    site_path = Path.cwd() / "public" / "site" / (path or "index.html")
+    if site_path.is_file():
+        return FileResponse(site_path, status_code=200)
+    return Response(status_code=404)
 
 
 mount_chainlit(app, "chat.py", "")
